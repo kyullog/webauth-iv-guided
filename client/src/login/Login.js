4a5735fd95064f403101a6a2d2ba8afd048da9a0
Jonathan Dorety <kyullog@gmail.com>
2019-04-03 21:58:32 -0400

    Create basic user and login components and set up routing w/ navbar

---
+++ client/src/login/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Login extends React.Component {
+  render() {
+    return (
+      <>
+        <h2>Login</h2>
+        <button>Login</button>
+      </>
+    );
+  }
+}
+
+export default Login;
---
+++ client/src/users/Users.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Users extends React.Component {
+  render() {
+    return (
+      <>
+        <h2>Users</h2>
+        <button>Users</button>
+      </>
+    );
+  }
+}
+
+export default Users;
